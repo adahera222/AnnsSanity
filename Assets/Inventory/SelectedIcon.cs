6f605481e7fe2d1c15e2a98eb1b728cde715c5f4
Shawn Pierre <shawn@origaminc.com>
2012-02-11 11:46:58 -0500

    initial frame for selected item

--- Assets/Inventory/SelectedIcon.cs
+++ Assets/Inventory/SelectedIcon.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 using System.Collections;
 
 public class SelectedIcon : MonoBehaviour {
-
+	
+	public GameObject frame;
+	
 	// Use this for initialization
 	void Start () 
 	{
+		//frame.renderer.enabled = false;
 		renderer.enabled = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
 }
